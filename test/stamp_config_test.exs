8fee987c020ab7f3e7cbb2a7ac40e4246786cd8c
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2026-08-08 04:29:34 -0300

    Make Config tests stricter by checking error messages.

--- test/stamp_config_test.exs
+++ test/stamp_config_test.exs
@@ -31,41 +31,53 @@ defmodule Stamp.StampConfigTest do
              } = Stamp.Config.new(params)
     end
 
-    test "raises without node fun" do
-      assert_raise ArgumentError, fn -> Stamp.Config.new() end
-    end
-
-    test "raises with node fun and 0 node bits" do
-      assert_raise ArgumentError, fn -> Stamp.Config.new(node_bits: 0, node_fun: &node_num/0) end
-    end
-
     test "raises without partition fun" do
-      assert_raise ArgumentError, fn ->
-        Stamp.Config.new(partition_bits: 2, node_bits: 7, node_fun: &node_num/0)
+      assert_raise ArgumentError, ~r"^partition_fun must be 0-arity function", fn ->
+        Stamp.Config.new(partition_bits: 2, node_bits: 5, node_fun: &node_num/0)
       end
     end
 
-    test "raises with partition fun and 0 part bits" do
-      assert_raise ArgumentError, fn ->
+    test "raises with partition fun and invalid part bits" do
+      assert_raise ArgumentError, ~r"^partition_bits must be > 0", fn ->
         Stamp.Config.new(partition_fun: &get_partition/0, node_fun: &node_num/0)
       end
-    end
-
-    test "raises with prefix and without codec" do
-      assert_raise ArgumentError, fn -> Stamp.Config.new(prefix: "f", node_fun: &node_num/0) end
-    end
-
-    test "raises with invalid zero bits" do
-      assert_raise ArgumentError, fn ->
-        Stamp.Config.new(time_bits: 0, node_fun: &node_num/0)
+      assert_raise ArgumentError, ~r"^partition_bits must be a non-negative integer", fn ->
+        Stamp.Config.new(partition_bits: -1, node_bits: 8, partition_fun: &get_partition/0, node_fun: &node_num/0)
       end
+    end
 
-      assert_raise ArgumentError, fn ->
-        Stamp.Config.new(sequence_bits: 0, node_fun: &node_num/0)
+    test "raises without node fun" do
+      assert_raise ArgumentError, ~r"^node_fun must be 0-arity function", fn ->
+        Stamp.Config.new()
+      end
+    end
+
+    test "raises with node fun and invalid node bits" do
+      assert_raise ArgumentError, "node_bits must be > 0 when node_fun is provided", fn ->
+        Stamp.Config.new(node_bits: 0, time_bits: 48, node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, ~r"^node_bits must be a non-negative integer", fn ->
+        Stamp.Config.new(node_bits: -1, time_bits: 49, node_fun: &node_num/0)
       end
     end
 
     test "raises with invalid bits" do
+      assert_raise ArgumentError, ~r"^time_bits must be a positive integer", fn ->
+        Stamp.Config.new(time_bits: 0, node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, ~r"^time_bits must be a positive integer", fn ->
+        Stamp.Config.new(time_bits: -1, node_bits: 8, node_fun: &node_num/0)
+      end
+
+      assert_raise ArgumentError, ~r"^sequence_bits must be a positive integer", fn ->
+        Stamp.Config.new(sequence_bits: 0, node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, ~r"^sequence_bits must be a positive integer", fn ->
+        Stamp.Config.new(sequence_bits: -1, node_bits: 8, node_fun: &node_num/0)
+      end
+    end
+
+    test "raises with invalid sum of bits" do
       params = [
         partition_bits: 4,
         time_bits: 41,
@@ -75,7 +87,42 @@ defmodule Stamp.StampConfigTest do
         node_fun: &node_num/0
       ]
 
-      assert_raise ArgumentError, fn -> Stamp.Config.new(params) end
+      assert_raise ArgumentError, ~r"^Sum of all bit parameters must be", fn ->
+        Stamp.Config.new(params)
+      end
+    end
+
+    test "raises with invalid epoch" do
+      assert_raise ArgumentError, ~r"^epoch must be a positive integer", fn ->
+        Stamp.Config.new(epoch: 0, node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, ~r"^epoch must be a positive integer", fn ->
+        Stamp.Config.new(epoch: -1, node_fun: &node_num/0)
+      end
+    end
+
+    test "raises with prefix and without codec" do
+      assert_raise ArgumentError, "codec is required when prefix is enabled", fn ->
+        Stamp.Config.new(prefix: "f", node_fun: &node_num/0)
+      end
+    end
+
+    test "raises with invalid prefix" do
+      assert_raise ArgumentError, "prefix must be a string", fn ->
+        Stamp.Config.new(prefix: 1, codec: Stamp.Codecs.Base62, node_fun: &node_num/0)
+      end
+    end
+
+    test "raises with invalid codec" do
+      assert_raise ArgumentError, "codec must be a module implementing Stamp.Codec behaviour", fn ->
+        Stamp.Config.new(codec: "codec", node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, "codec must be a module implementing Stamp.Codec behaviour", fn ->
+        Stamp.Config.new(codec: Stamp.Codec, node_fun: &node_num/0)
+      end
+      assert_raise ArgumentError, "could not load module :nonexisting due to reason :nofile", fn ->
+        Stamp.Config.new(codec: :nonexisting, node_fun: &node_num/0)
+      end
     end
   end
 end
