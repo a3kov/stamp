a1ec39ca89a0fd5eddc269a8b10c4197f49bc28a
Andrey Tretyakov <813860+a3kov@users.noreply.github.com>
2026-08-15 07:51:03 -0300

    Fix partition range test.

--- test/stamp_config_test.exs
+++ test/stamp_config_test.exs
@@ -145,13 +145,11 @@ defmodule Stamp.StampConfigTest do
                    end
     end
 
-    test "Config.partitions/1 returns correct list of partitions" do
-      assert [
-               {0, 2_305_843_009_213_693_952},
-               {2_305_843_009_213_693_952, 4_611_686_018_427_387_904},
-               {4_611_686_018_427_387_904, 6_917_529_027_641_081_856},
-               {6_917_529_027_641_081_856, 9_223_372_036_854_775_808}
-             ] = Config.partitions(2)
+    test "Config.partition_range/2 returns correct range" do
+      assert {0, 2_305_843_009_213_693_952} = Config.partition_range(0, 2)
+      assert {2_305_843_009_213_693_952, 4_611_686_018_427_387_904} = Config.partition_range(1, 2)
+      assert {4_611_686_018_427_387_904, 6_917_529_027_641_081_856} = Config.partition_range(2, 2)
+      assert {6_917_529_027_641_081_856, 9_223_372_036_854_775_808} = Config.partition_range(3, 2)
     end
   end
 end
